2e377f8b886a183f2f48ccd9e7f4a2a60a4df3cd
yayanrw <yayanraw@gmail.com>
2021-11-28 07:23:51 +0700

    Add project files.

---
+++ TodoAPI/Models/TodoModel.cs
@@ -0,0 +1,14 @@
+﻿namespace TodoAPI.Models
+{
+    public class TodoModel
+    {
+        public Guid pid { get; set; }
+        public String? title { get; set; }
+        public String? description { get; set; }
+        public DateTime date { get; set; }
+        public DateTime created_at { get; set; }
+        public String? created_by { get; set; }
+        public DateTime updated_at { get; set; }
+        public String? updated_by { get; set; }
+    }
+}
