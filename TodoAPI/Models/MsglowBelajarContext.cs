b0bfb3251abec0e22b676b31c59d967b59745244
yayanrw <yayanraw@gmail.com>
2021-11-28 14:04:20 +0700

    add context

---
+++ TodoAPI/Models/MsglowBelajarContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoAPI.Models
+{
+    public class MsglowBelajarContext : DbContext
+    {
+        public MsglowBelajarContext(DbContextOptions options) : base(options)
+        {}
+
+        public DbSet<TodoModel> Todos { get; set; }
+    }
+}
