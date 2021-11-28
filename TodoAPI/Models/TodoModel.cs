fe111f7ab6e7627bef36298321dba9b01dbad7cf
yayanrw <yayanraw@gmail.com>
2021-11-28 14:05:10 +0700

    add todo model

--- TodoAPI/Models/TodoModel.cs
+++ TodoAPI/Models/TodoModel.cs
@@ -1,14 +1,19 @@
-﻿namespace TodoAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TodoAPI.Models
 {
+    [Table("t_todos")]
     public class TodoModel
     {
+        [Key]
         public Guid pid { get; set; }
         public String? title { get; set; }
         public String? description { get; set; }
-        public DateTime date { get; set; }
-        public DateTime created_at { get; set; }
+        public DateTime? date { get; set; }
+        public DateTime? created_at { get; set; }
         public String? created_by { get; set; }
-        public DateTime updated_at { get; set; }
+        public DateTime? updated_at { get; set; }
         public String? updated_by { get; set; }
     }
 }
