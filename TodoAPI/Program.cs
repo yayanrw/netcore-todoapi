5116e1cbbaa4c2efb703a52e46a035ca68124864
yayanrw <yayanraw@gmail.com>
2021-12-24 06:15:08 +0700

    add service get url

--- TodoAPI/Program.cs
+++ TodoAPI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using TodoAPI.Models;
+using TodoAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +10,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-   
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSingleton<IUriService>(o =>
+{
+    var accessor = o.GetRequiredService<IHttpContextAccessor>();
+    var request = accessor.HttpContext.Request;
+    var uri = string.Concat(request.Scheme, "://", request.Host.ToUriComponent());
+    return new UriService(uri);
+});
+
 string connString = builder.Configuration.GetConnectionString("MsglowBelajar");
 builder.Services.AddDbContext<MsglowBelajarContext>(options =>
 options.UseMySql(connString, new MySqlServerVersion(new Version(10, 1, 40))));
