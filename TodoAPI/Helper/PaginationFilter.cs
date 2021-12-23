ad078db87bf9f4466713c5e598b7b87f744ffa17
yayanrw <yayanraw@gmail.com>
2021-12-24 06:22:43 +0700

    change default max pagesize

--- TodoAPI/Helper/PaginationFilter.cs
+++ TodoAPI/Helper/PaginationFilter.cs
@@ -7,12 +7,12 @@
         public PaginationFilter()
         {
             this.PageNumber = 1;
-            this.PageSize = 10;
+            this.PageSize = 100;
         }
         public PaginationFilter(int pageNumber, int pageSize)
         {
             this.PageNumber = pageNumber < 1 ? 1 : pageNumber;
-            this.PageSize = pageSize > 10 ? 10 : pageSize;
+            this.PageSize = pageSize > 100 ? 100 : pageSize;
         }
     }
 }
